0cf91768edb6b40d99d04091fd92f532e191f2c7
Ken Wilcox <wilcoxk@medamerica.com>
2016-05-14 22:41:01 -0600

    Returning individual properties

--- Source/AirVinyl.API/Controllers/PeopleController.cs
+++ Source/AirVinyl.API/Controllers/PeopleController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.OData;
+using System.Web.OData.Routing;
 using AirVinyl.DataAccessLayer;
+using AirVinyl.API.Helpers;
 
 namespace AirVinyl.API.Controllers
 {
@@ -26,6 +28,35 @@ namespace AirVinyl.API.Controllers
             }
 
             return Ok(person);
+        }
+
+        [HttpGet]
+        [ODataRoute("People({key})/Email")]
+        [ODataRoute("People({key})/FirstName")]
+        [ODataRoute("People({key})/LastName")]
+        [ODataRoute("People({key})/DateOfBirth")]
+        [ODataRoute("People({key})/Gender")]
+        public IHttpActionResult GetPersonProperty([FromODataUri] int key)
+        {
+            var person = _context.People.FirstOrDefault(p => p.PersonId == key);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            var propertyToGet = Url.Request.RequestUri.Segments.Last();
+            if (!person.HasProperty(propertyToGet))
+            {
+                return NotFound();
+            }
+
+            var propertyValue = person.GetValue(propertyToGet);
+            if (propertyValue == null)
+            {
+                return StatusCode(System.Net.HttpStatusCode.NoContent);
+            }
+
+            return this.CreateOKHttpActionResult(propertyValue);
         }
 
         protected override void Dispose(bool disposing)
