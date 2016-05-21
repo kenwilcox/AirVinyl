328f59f51735cfc30f511b37023f6fc278861b95
Ken Wilcox <wilcoxk@medamerica.com>
2016-05-20 21:58:37 -0600

    Added Support for RecordStore

---
+++ Source/AirVinyl.API/Controllers/RecordStoresController.cs
@@ -0,0 +1,61 @@
+﻿using AirVinyl.API.Helpers;
+using AirVinyl.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.OData;
+using System.Web.OData.Routing;
+
+namespace AirVinyl.API.Controllers
+{
+    public class RecordStoresController: ODataController
+    {
+        private AirVinylDbContext _context = new AirVinylDbContext();
+
+        // GET odata/RecordStores
+        [EnableQuery]
+        public IHttpActionResult Get()
+        {
+            return Ok(_context.RecordStores);
+        }
+
+        // GET odata/RecordStores(key)
+        [EnableQuery]
+        public IHttpActionResult Get([FromODataUri] int key)
+        {
+            var recordStores = _context.RecordStores.Where(r => r.RecordStoreId == key);
+            if (!recordStores.Any())
+            {
+                return NotFound();
+            }
+            return Ok(SingleResult.Create(recordStores));
+        }
+
+        [HttpGet]
+        [ODataRoute("RecordStores({key})/Tags")]
+        [EnableQuery]
+        public IHttpActionResult GetRecordStoreTagsProperty([FromODataUri] int key)
+        {
+            // no include necessary for EF - Tags ins't a navigation property
+            // in the entity model
+            var recordStore = _context.RecordStores.FirstOrDefault(r => r.RecordStoreId == key);
+            if (recordStore == null)
+            {
+                return NotFound();
+            }
+            var collectionPropertyToGet = Url.Request.RequestUri.Segments.Last();
+            var collectionPropertyValue = recordStore.GetValue(collectionPropertyToGet);
+
+            // return the collection of tags
+            return this.CreateOKHttpActionResult(collectionPropertyValue);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
--- Source/AirVinyl.Model/RecordStore.cs
+++ Source/AirVinyl.Model/RecordStore.cs
@@ -10,6 +10,8 @@ namespace AirVinyl.Model
 {
     public class RecordStore
     {
+        private ICollection<string> _tags;
+
         [Key]
         public int RecordStoreId { get; set; }
 
@@ -19,7 +21,23 @@ namespace AirVinyl.Model
 
         public Address StoreAddress { get; set; }
 
-        public ICollection<string> Tags { get; set; }
+        public ICollection<string> Tags { get { return _tags; } set { _tags = value; } }
+
+        public string TagsAsString
+        {
+            get
+            {
+                if (_tags == null || !_tags.Any())
+                {
+                    return "";
+                }
+                return _tags.Aggregate((a, b) => a + "," + b);
+            }
+            set
+            {
+                _tags = value.Split(',').ToList();
+            }
+        }
 
         public ICollection<Rating> Ratings { get; set; }
 
